2d6788ea9b53e6bbdffda9b34fa370f2621e189e
MNuenninghoff <mikenue@gmail.com>
2024-02-11 15:06:57 -0600

    getters/setter tostring and constructor for Mannerisms

--- src/main/java/com/mnuenninghoff/entity/Mannerisms.java
+++ src/main/java/com/mnuenninghoff/entity/Mannerisms.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Mannerisms.
+ */
 public class Mannerisms {
+    private int id;
+    private String mannerisms;
+
+    /**
+     * Instantiates a new Mannerisms.
+     */
+    public Mannerisms() {
+
+    }
+
+    /**
+     * returns a string representation of the mannerisms
+     * @return a string representation of the mannerisms
+     */
+    public String toString() {
+        String mannerisms = "id: " + id + " mannerisms: " + this.mannerisms;
+        return mannerisms;
+    }
+
+    /**
+     * Gets mannerisms.
+     *
+     * @return the mannerisms
+     */
+    public String getMannerisms() {
+        return mannerisms;
+    }
+
+    /**
+     * Sets mannerisms.
+     *
+     * @param mannerisms the mannerisms
+     */
+    public void setMannerisms(String mannerisms) {
+        this.mannerisms = mannerisms;
+    }
 }
