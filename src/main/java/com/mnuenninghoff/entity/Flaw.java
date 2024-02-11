958c85a96e5cdc0dd7e39c0fc540f690f05128c8
MNuenninghoff <mikenue@gmail.com>
2024-02-11 14:58:26 -0600

    getters/setter tostring and constructor for Flaw

--- src/main/java/com/mnuenninghoff/entity/Flaw.java
+++ src/main/java/com/mnuenninghoff/entity/Flaw.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Flaw.
+ */
 public class Flaw {
+    private int id;
+    private String flaw;
+
+    /**
+     * Instantiates a new Flaw.
+     */
+    public Flaw() {
+
+    }
+
+    /**
+     * returns a string represenatation of the flaw
+     * @return a string represenatation of the flaw
+     */
+    public String toString() {
+        String flaw = "id: " + id + " flaw: " + this.flaw;
+        return flaw;
+    }
+
+    /**
+     * Gets flaw.
+     *
+     * @return the flaw
+     */
+    public String getFlaw() {
+        return flaw;
+    }
+
+    /**
+     * Sets flaw.
+     *
+     * @param flaw the flaw
+     */
+    public void setFlaw(String flaw) {
+        this.flaw = flaw;
+    }
 }
