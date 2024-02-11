3b877ef9886ee20d83dc6ba9153fe4f54eca3684
MNuenninghoff <mikenue@gmail.com>
2024-02-11 15:09:34 -0600

    getters/setter tostring and constructor for Race

--- src/main/java/com/mnuenninghoff/entity/Race.java
+++ src/main/java/com/mnuenninghoff/entity/Race.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Race.
+ */
 public class Race {
+    private int id;
+    private String race;
+
+    /**
+     * Instantiates a new Race.
+     */
+    public Race() {
+
+    }
+
+    /**
+     * returns a string representation of the Race
+     * @return a string representation of the Race
+     */
+    public String toString() {
+        String race = "id: " + id + " race: " + this.race;
+        return race;
+    }
+
+    /**
+     * Gets race.
+     *
+     * @return the race
+     */
+    public String getRace() {
+        return race;
+    }
+
+    /**
+     * Sets race.
+     *
+     * @param race the race
+     */
+    public void setRace(String race) {
+        this.race = race;
+    }
 }
