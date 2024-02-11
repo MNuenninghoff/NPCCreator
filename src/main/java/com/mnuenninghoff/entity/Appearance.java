62de0d1d3247c2e0819bb100375153a78f756c7a
MNuenninghoff <mikenue@gmail.com>
2024-02-11 14:29:00 -0600

    added getters and setters and to string and constructor to Appearance

--- src/main/java/com/mnuenninghoff/entity/Appearance.java
+++ src/main/java/com/mnuenninghoff/entity/Appearance.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Appearance.
+ */
 public class Appearance {
+    private int id;
+    private String appearance;
+
+    /**
+     * Instantiates a new Appearance.
+     */
+    public Appearance() {
+
+    }
+
+    /**
+     * Gets appearance.
+     *
+     * @return the appearance
+     */
+    public String getAppearance() {
+        return appearance;
+    }
+
+    /**
+     * Sets appearance.
+     *
+     * @param appearance the appearance
+     */
+    public void setAppearance(String appearance) {
+        this.appearance = appearance;
+    }
+
+    /**
+     * returns a string representation of the appearance
+     * @return
+     */
+    public String toString() {
+        String appearance = "id: " + id + "appearance: " + this.appearance;
+        return appearance;
+    }
 }
