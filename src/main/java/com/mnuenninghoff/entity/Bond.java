6a2d9af22a034cd8439a7c054abc8f8e6a919def
MNuenninghoff <mikenue@gmail.com>
2024-02-11 14:54:36 -0600

    getters/setter tostring and constructor for Bond

--- src/main/java/com/mnuenninghoff/entity/Bond.java
+++ src/main/java/com/mnuenninghoff/entity/Bond.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Bond.
+ */
 public class Bond {
+    private int id;
+    private String bond;
+
+    /**
+     * Instantiates a new Bond.
+     */
+    public Bond(){
+
+    }
+
+    /**
+     * Gets bond.
+     *
+     * @return the bond
+     */
+    public String getBond() {
+        return bond;
+    }
+
+    /**
+     * Sets bond.
+     *
+     * @param bond the bond
+     */
+    public void setBond(String bond) {
+        this.bond = bond;
+    }
+
+    /**
+     * returns a string representation of the bond
+     * @return string representation of a bond
+     */
+    public String toString(){
+        String bond = "id: " + "bond: " + this.bond;
+        return bond;
+    }
 }
