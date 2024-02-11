3ea39870f8ff168b0dd3943a7b6e9a225f9ea08f
MNuenninghoff <mikenue@gmail.com>
2024-02-11 15:13:25 -0600

    getters/setter tostring and constructor for Talent

--- src/main/java/com/mnuenninghoff/entity/Talent.java
+++ src/main/java/com/mnuenninghoff/entity/Talent.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Talent.
+ */
 public class Talent {
+    private int id;
+    private String talent;
+
+    /**
+     * Instantiates a new Talent.
+     */
+    public Talent() {
+
+    }
+
+    /**
+     * returns a string represenation of the talent
+     * @return a string represenation of the talent
+     */
+    public String toString() {
+        String talent = "id: " + id + " talent: " + this.talent;
+        return talent;
+    }
+
+    /**
+     * Gets talent.
+     *
+     * @return the talent
+     */
+    public String getTalent() {
+        return talent;
+    }
+
+    /**
+     * Sets talent.
+     *
+     * @param talent the talent
+     */
+    public void setTalent(String talent) {
+        this.talent = talent;
+    }
 }
