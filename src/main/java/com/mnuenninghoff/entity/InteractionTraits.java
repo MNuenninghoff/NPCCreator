48fe0647bb4b549889f21bb1a27dbfc972d926be
MNuenninghoff <mikenue@gmail.com>
2024-02-11 15:03:10 -0600

    getters/setter tostring and constructor for InteractionTraits

--- src/main/java/com/mnuenninghoff/entity/InteractionTraits.java
+++ src/main/java/com/mnuenninghoff/entity/InteractionTraits.java
@@ -1,4 +1,43 @@
 package com.mnuenninghoff.entity;
 
+/**
+ * The type Interaction traits.
+ */
 public class InteractionTraits {
+    private int id;
+    private String interactionTraits;
+
+    /**
+     * Instantiates a new Interaction traits.
+     */
+    public InteractionTraits() {
+
+    }
+
+    /**
+     * returns a string representation of the interaction traits
+     * @return a string representation of the interaction traits
+     */
+    public String toString() {
+        String interactionTraits = "id: " + id + " interaction traits: " + this.interactionTraits;
+        return interactionTraits;
+    }
+
+    /**
+     * Gets interaction traits.
+     *
+     * @return the interaction traits
+     */
+    public String getInteractionTraits() {
+        return interactionTraits;
+    }
+
+    /**
+     * Sets interaction traits.
+     *
+     * @param interactionTraits the interaction traits
+     */
+    public void setInteractionTraits(String interactionTraits) {
+        this.interactionTraits = interactionTraits;
+    }
 }
