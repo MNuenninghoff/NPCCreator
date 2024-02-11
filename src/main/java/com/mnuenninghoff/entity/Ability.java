284a245b65db5548ddc2ad312c9b1507d7b3e4e2
MNuenninghoff <mikenue@gmail.com>
2024-02-11 14:25:52 -0600

    added instance variables and getters/setters to Ability class

--- src/main/java/com/mnuenninghoff/entity/Ability.java
+++ src/main/java/com/mnuenninghoff/entity/Ability.java
@@ -1,4 +1,18 @@
 package com.mnuenninghoff.entity;
 
 public class Ability {
+    private int id;
+    private String ability;
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+    public String toString() {
+        String ability = "id: " + id + "ability: " + this.ability;
+        return ability;
+    }
 }
