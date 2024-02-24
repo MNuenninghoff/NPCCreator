97143904cfb3ed038c9aa6b35c963da303e2fb14
MNuenninghoff <mikenue@gmail.com>
2024-02-24 08:55:23 -0600

    added Hibernate annotations to Ability

--- src/main/java/com/mnuenninghoff/persistence/GenericDao.java
+++ src/main/java/com/mnuenninghoff/persistence/GenericDao.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import edu.matc.entity.Order;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Predicate;
