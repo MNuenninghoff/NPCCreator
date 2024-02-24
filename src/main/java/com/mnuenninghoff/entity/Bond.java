322cf1abb5f235b92cd64b4a0e4127f9f2cf55fc
MNuenninghoff <mikenue@gmail.com>
2024-02-24 09:05:33 -0600

    added Hibernate annotations for Bond

--- src/main/java/com/mnuenninghoff/entity/Bond.java
+++ src/main/java/com/mnuenninghoff/entity/Bond.java
@@ -1,17 +1,77 @@
 package com.mnuenninghoff.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * The type Bond.
  */
+@Entity(name = "Bond")
+@Table(name = "bond")
 public class Bond {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+    @Column(name = "bond")
     private String bond;
+    @OneToMany(mappedBy = "bond", cascade = CascadeType.ALL, orphanRemoval = false, fetch = FetchType.EAGER)
+    private List<NPC> npcs = new ArrayList<>();
 
     /**
      * Instantiates a new Bond.
      */
     public Bond(){
 
+    }
+
+    /**
+     * Instantiates a new Bond.
+     *
+     * @param bond the bond
+     */
+    public Bond(String bond) {
+        this.bond = bond;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets npcs.
+     *
+     * @return the npcs
+     */
+    public List<NPC> getNpcs() {
+        return npcs;
+    }
+
+    /**
+     * Sets npcs.
+     *
+     * @param npcs the npcs
+     */
+    public void setNpcs(List<NPC> npcs) {
+        this.npcs = npcs;
     }
 
     /**
@@ -39,5 +99,18 @@ public class Bond {
     public String toString(){
         String bond = "id: " + "bond: " + this.bond;
         return bond;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bond bond1 = (Bond) o;
+        return id == bond1.id && Objects.equals(bond, bond1.bond) && Objects.equals(npcs, bond1.npcs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, bond, npcs);
     }
 }
