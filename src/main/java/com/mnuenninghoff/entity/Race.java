ad054b3152dfb047bc48326452b65488e5fba6aa
MNuenninghoff <mikenue@gmail.com>
2024-02-24 10:48:09 -0600

    added hibernate annotations to Race

--- src/main/java/com/mnuenninghoff/entity/Race.java
+++ src/main/java/com/mnuenninghoff/entity/Race.java
@@ -1,17 +1,77 @@
 package com.mnuenninghoff.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * The type Race.
  */
+@Entity(name = "Race")
+@Table(name = "race")
 public class Race {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+    @Column(name = "race")
     private String race;
+    @OneToMany(mappedBy = "race", cascade = CascadeType.ALL, orphanRemoval = false, fetch = FetchType.EAGER)
+    private List<NPC> npcs = new ArrayList<>();
 
     /**
      * Instantiates a new Race.
      */
     public Race() {
 
+    }
+
+    /**
+     * Instantiates a new Race.
+     *
+     * @param race the race
+     */
+    public Race(String race) {
+        this.race = race;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets npcs.
+     *
+     * @return the npcs
+     */
+    public List<NPC> getNpcs() {
+        return npcs;
+    }
+
+    /**
+     * Sets npcs.
+     *
+     * @param npcs the npcs
+     */
+    public void setNpcs(List<NPC> npcs) {
+        this.npcs = npcs;
     }
 
     /**
@@ -39,5 +99,18 @@ public class Race {
      */
     public void setRace(String race) {
         this.race = race;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Race race1 = (Race) o;
+        return id == race1.id && Objects.equals(race, race1.race) && Objects.equals(npcs, race1.npcs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, race, npcs);
     }
 }
