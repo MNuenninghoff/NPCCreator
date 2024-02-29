6970d7d57040763ce8094fe7575a034f794b06cc
MNuenninghoff <mikenue@gmail.com>
2024-02-28 20:27:48 -0600

    generated JavaDoc for NPC

--- src/main/java/com/mnuenninghoff/entity/NPC.java
+++ src/main/java/com/mnuenninghoff/entity/NPC.java
@@ -56,66 +56,146 @@ public class NPC {
 
     }
 
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets user.
+     *
+     * @param user the user
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Gets talent.
+     *
+     * @return the talent
+     */
     public Talent getTalent() {
         return talent;
     }
 
+    /**
+     * Sets talent.
+     *
+     * @param talent the talent
+     */
     public void setTalent(Talent talent) {
         this.talent = talent;
     }
 
+    /**
+     * Gets race.
+     *
+     * @return the race
+     */
     public Race getRace() {
         return race;
     }
 
+    /**
+     * Sets race.
+     *
+     * @param race the race
+     */
     public void setRace(Race race) {
         this.race = race;
     }
 
+    /**
+     * Gets mannerisms.
+     *
+     * @return the mannerisms
+     */
     public Mannerisms getMannerisms() {
         return mannerisms;
     }
 
+    /**
+     * Sets mannerisms.
+     *
+     * @param mannerisms the mannerisms
+     */
     public void setMannerisms(Mannerisms mannerisms) {
         this.mannerisms = mannerisms;
     }
 
+    /**
+     * Gets flaw.
+     *
+     * @return the flaw
+     */
     public Flaw getFlaw() {
         return flaw;
     }
 
+    /**
+     * Sets flaw.
+     *
+     * @param flaw the flaw
+     */
     public void setFlaw(Flaw flaw) {
         this.flaw = flaw;
     }
 
+    /**
+     * Gets bond.
+     *
+     * @return the bond
+     */
     public Bond getBond() {
         return bond;
     }
 
+    /**
+     * Sets bond.
+     *
+     * @param bond the bond
+     */
     public void setBond(Bond bond) {
         this.bond = bond;
     }
 
+    /**
+     * Gets appearance.
+     *
+     * @return the appearance
+     */
     public Appearance getAppearance() {
         return appearance;
     }
 
+    /**
+     * Sets appearance.
+     *
+     * @param appearance the appearance
+     */
     public void setAppearance(Appearance appearance) {
         this.appearance = appearance;
     }
 
+    /**
+     * Gets ability.
+     *
+     * @return the ability
+     */
     public Ability getAbility() {
         return ability;
     }
 
+    /**
+     * Sets ability.
+     *
+     * @param ability the ability
+     */
     public void setAbility(Ability ability) {
         this.ability = ability;
     }
