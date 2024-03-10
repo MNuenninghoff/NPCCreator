80bffcd11519fcc649af04784c9419043d490e4f
MNuenninghoff <mikenue@gmail.com>
2024-03-09 20:55:37 -0600

    created empty test class for IronArachneDao

---
+++ src/test/java/com/mnuenninghoff/persistence/IronArachneDaoTest.java
@@ -0,0 +1,20 @@
+package com.mnuenninghoff.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IronArachneDaoTest {
+
+    @Test
+    void getMaleFirstName() {
+    }
+
+    @Test
+    void getFemaleFirstName() {
+    }
+
+    @Test
+    void getFamilyName() {
+    }
+}
