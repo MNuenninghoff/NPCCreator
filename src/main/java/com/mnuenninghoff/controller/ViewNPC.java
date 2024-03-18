c74e8a0e5221affd51d15e7a5ba33fe9dab5962f
MNuenninghoff <mikenue@gmail.com>
2024-03-17 19:37:11 -0500

    wrote doGet in ViewNPC to search for NPCs generated by current user

--- src/main/java/com/mnuenninghoff/controller/ViewNPC.java
+++ src/main/java/com/mnuenninghoff/controller/ViewNPC.java
@@ -1,8 +1,12 @@
 package com.mnuenninghoff.controller;
 
+import com.mnuenninghoff.entity.NPC;
+import com.mnuenninghoff.entity.User;
+import com.mnuenninghoff.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Searches database for all NPCs created by a user, sends the returned list to
@@ -31,7 +36,17 @@ public class ViewNPC extends HttpServlet {
      */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         HttpSession session = request.getSession();
+        GenericDao<NPC> npcDao = new GenericDao<NPC>(NPC.class);
+        User user = (User)session.getAttribute("user");
+
+        // search database for all NPCs created by user. All the list of NPCs to the session
+        List<NPC> npcs = npcDao.findByPropertyEqual("user", user);
+        session.setAttribute("npcs", npcs);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("viewNPCs.jsp");
+        dispatcher.forward(request, response);
     }
 }
 
