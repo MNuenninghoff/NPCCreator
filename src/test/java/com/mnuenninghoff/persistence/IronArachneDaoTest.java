dcffbe8fdd7238e7cf2c8712283ec80a79f42a3b
MNuenninghoff <mikenue@gmail.com>
2024-05-05 11:43:30 -0500

    updated IronArachneDaoTest

--- src/test/java/com/mnuenninghoff/persistence/IronArachneDaoTest.java
+++ src/test/java/com/mnuenninghoff/persistence/IronArachneDaoTest.java
@@ -19,46 +19,47 @@ class IronArachneDaoTest {
     }
     @Test
     void getMaleFirstName() {
-        Name maleName = dao.getMaleFirstName("dwarf");
-        logger.debug(maleName);
+        //male name
+        Name maleName = dao.getName("dwarf", "male");
+        logger.debug("Male Name valid race: " + maleName);
         assertEquals(1, maleName.getCount());
         assertEquals(1, maleName.getNames().size());
     }
 
     @Test
     void getMaleFirstNameInvalidRace() {
-        Name maleName = dao.getMaleFirstName("aasimar");
-        logger.debug(maleName);
+        Name maleName = dao.getName("aasimar", "male");
+        logger.debug("Male Name invalid race: " + maleName);
         assertEquals(1, maleName.getCount());
         assertEquals(1, maleName.getNames().size());
     }
 
     @Test
     void getFemaleFirstName() {
-        Name femaleName = dao.getFemaleFirstName("dwarf");
-        logger.debug(femaleName);
+        Name femaleName = dao.getName("dwarf", "female");
+        logger.debug("Female Name : " + femaleName);
         assertEquals(1, femaleName.getCount());
         assertEquals(1, femaleName.getNames().size());
     }
     @Test
     void getFemaleFirstNameInvalidRace() {
-        Name maleName = dao.getFemaleFirstName("aasimar");
-        logger.debug(maleName);
-        assertEquals(1, maleName.getCount());
-        assertEquals(1, maleName.getNames().size());
+        Name femaleName = dao.getName("aasimar", "female");
+        logger.debug("female name invalid race: " + femaleName);
+        assertEquals(1, femaleName.getCount());
+        assertEquals(1, femaleName.getNames().size());
     }
     @Test
     void getFamilyNameInvalidRace() {
-        Name maleName = dao.getFamilyName("aasimar");
-        logger.debug(maleName);
-        assertEquals(1, maleName.getCount());
-        assertEquals(1, maleName.getNames().size());
+        Name familyName = dao.getName("aasimar", "family");
+        logger.debug("Family name invalid race : " + familyName);
+        assertEquals(1, familyName.getCount());
+        assertEquals(1, familyName.getNames().size());
     }
 
     @Test
     void getFamilyName() {
-        Name familyName = dao.getFemaleFirstName("dwarf");
-        logger.debug(familyName);
+        Name familyName = dao.getName("dwarf", "family");
+        logger.debug("Family name: " + familyName);
         assertEquals(1, familyName.getCount());
         assertEquals(1, familyName.getNames().size());
     }
