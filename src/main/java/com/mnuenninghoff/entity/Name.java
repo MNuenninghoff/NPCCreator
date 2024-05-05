bc830581312c2921e79c92bb36a96bb4d6a1d335
MNuenninghoff <mikenue@gmail.com>
2024-05-05 14:24:55 -0500

    generated javadoc for Name

--- src/main/java/com/mnuenninghoff/entity/Name.java
+++ src/main/java/com/mnuenninghoff/entity/Name.java
@@ -3,6 +3,9 @@ package com.mnuenninghoff.entity;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * The type Name.
+ */
 public class Name{
 
 	@JsonProperty("names")
@@ -11,18 +14,38 @@ public class Name{
 	@JsonProperty("count")
 	private int count;
 
+	/**
+	 * Set names.
+	 *
+	 * @param names the names
+	 */
 	public void setNames(List<String> names){
 		this.names = names;
 	}
 
+	/**
+	 * Get names list.
+	 *
+	 * @return the list
+	 */
 	public List<String> getNames(){
 		return names;
 	}
 
+	/**
+	 * Set count.
+	 *
+	 * @param count the count
+	 */
 	public void setCount(int count){
 		this.count = count;
 	}
 
+	/**
+	 * Get count int.
+	 *
+	 * @return the int
+	 */
 	public int getCount(){
 		return count;
 	}
